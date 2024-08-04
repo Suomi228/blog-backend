de229f2ab25b3c2197a388c52b406ade39e10cdd
Suomi228 <bixep345@gmail.com>
2024-08-04 16:19:45 +0300

    registration mongoDB

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ import mongoose from "mongoose";
 import { registerValidation } from "./validations/auth.js";
 import {validationResult} from 'express-validator'
 import dotenv from 'dotenv'
-
-
+import UserModel from './models/User.js'
+import bcrypt from 'bcrypt';
 
 
 dotenv.config();
-const MONG_DB = process.env.MONG_DB 
+
 
 const app = express();
 app.use(express.json());
@@ -21,19 +21,38 @@ app.get('/', (req, res) => {
  });
 
 
- app.post('/auth/register',registerValidation, (req, res) => {
+ app.post('/auth/register',registerValidation, async (req, res) => {
 
+   try {
+    
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     };
-    res.json('User created');
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+    const data = new UserModel(
+        {
+            name: req.body.name,
+            email: req.body.email,
+            passwordHash,
+            avatarUrl: req.body.avatarUrl
+        }
+    )
+
+    const user = await data.save();
+    res.json(user);
+   } catch (error) {
+    console.log(error);
+    res.status(500).send('Server Error');
+   }
     
  });
  app.listen(PORT, (req, res) =>{
      console.log(`Server is running on port ${PORT}`);
  });
  mongoose
-.connect(MONG_DB)
+.connect(process.env.MONG_DB)
 .then(()=>{console.log('DB OK')})
 .catch(err=>{console.log(err)});
